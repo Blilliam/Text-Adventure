b20876a70380a0ceecfbb1dd64b496d77382c128
Blilliam <jyscudder@gmail.com>
2025-08-21 18:25:53 -0400

    added map display

--- file.js
+++ file.js
@@ -7,37 +7,20 @@ const player = {
 }
 const input = document.querySelector("#text-controls")
 input.addEventListener("keypress", function(e) {
-    console.log(e)
-    if (e.key == "Enter"){
-        console.log(input.value)
-        console.log(xycheck(0, 1))
-        console.log(xycheck(0, 0))
-        console.log(xycheck(0, -1))
-        console.log(xycheck(0, 10))
-        if (e.key == "w") {
-            if (xycheck(0, 1)) {
-                player.y += 1
-            }
-        }
-        if (e.key == "a") {
-            if (xycheck(-1, 0)) {
-                player.x--
-            }
-        }
-        if (e.key == "s") {
-            if (xycheck(0, -1)) {
-                player.y--
-            }
-        }
-        if (e.key == "d") {
-            if (xycheck(1, 0)) {
-                player.x++
-            }
-        }
-        console.log(player.x, player.y)
-        input.value = ""
+    if (input.value == "w") {
+        move(0, -1)
     }
-})
+    if (input.value == "a") {
+        move(-1, 0)
+    }
+    if (input.value == "s") {
+        move(0, 1)
+    }
+    if (input.value == "d") {
+        move(1, 0)
+    }
+}
+)
 const map = []
 for (let row = 0;row < 10; row++) {
     map.push([])
@@ -50,12 +33,60 @@ for (let row = 0;row < 10; row++) {
 function room() {
     return {enemies: [],
             items: [],
-            name: "empty"
+            name: " ~ ",
             }
 }
 
-map[5][5].name = 'Shop'
-console.log(map)
+function move(dx, dy) {
+    if (xycheck(dx, dy)) {
+        player.x += dx
+        player.y += dy
+        input.value = ""
+        displayMap()
+    }
+}
+
+function hall(name, map, x, y) {
+    map[x][y].name = name
+    map[x + 1][y].name = "Hall"
+    map[x - 1][y].name = "Hall"
+    map[x][y + 1].name = "Hall"
+    map[x][y - 1].name = "Hall"
+}
+
+
+hall('Weapon Shop', map, 5 ,5)
+hall('Item Shop', map, 1, 1)
+hall('Field', map, 7, 8)
+hall('Aurmor Shop', map, 2, 7)
+hall('Lodge', map, 8, 1)
+hall('Old House', map, 4, 2)
+hall('Forge', map, 1, 4)
+hall('Cave', map, 8, 4)
+
+function displayMap() {
+    let colStr = "    "
+    for (let i = 0; i < map.length; i++) {
+        colStr += i + "    "
+    }
+    console.log(colStr)
+
+    for (let i = 0; i < map.length; i++) {
+        let rowStr = i + "  "
+        for (let j = 0; j < map[i].length; j++){
+            if (player.x == j && player.y == i) {
+                rowStr += "PLR"
+            }
+            else {
+                rowStr += map[i][j].name.substring(0, 3)
+            }
+            rowStr += "  "
+        }
+        console.log(rowStr)
+    }
+}
+
+displayMap()    
 
 function xycheck(dx, dy) {
     let x = player.x + dx
